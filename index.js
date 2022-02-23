2845e89e072e78446bf8ac3c934a1509e7585cf8
Pavan S <pavans.shetty.2452@gmail.com>
2022-02-23 15:17:22 +0530

    change http

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Composer } = require('micro-bot')
-const request = require('request');
+const https = require('https');
 const bot = new Composer()
 
 
@@ -45,11 +45,24 @@ bot.hears('hi', (ctx) => {
     });
 
     bot.action('dog', ctx => {
-        request('https://rnsit-mca-miniproject.herokuapp.com/latest', { json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
-            ctx.reply(body.explanation)
-            console.log(body.explanation);
-          });
+        https.get('https://rnsit-mca-miniproject.herokuapp.com/latest', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).date);
+    console.log(JSON.parse(data).temp);
+    ctx.reply('last Update on: '+ JSON.parse(data).date +'\nTemprature: '+ JSON.parse(data).temp)
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
     })
     bot.action('cat', ctx => {
         ctx.reply('cat👍')
