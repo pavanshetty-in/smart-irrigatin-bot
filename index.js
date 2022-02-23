ec92c7e9add03186f1a43312f4785a5d62d81e21
Pavan S <pavans.shetty.2452@gmail.com>
2022-02-23 14:31:59 +0530

    options added

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const { Composer } = require('micro-bot')
 const bot = new Composer()
 
 bot.start((ctx) => ctx.reply('Welcome by pavan Shetty'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => {
+     ctx.reply('Hey there',{
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: "dog",
+                        callback_data: 'dog'
+                    },
+                    {
+                        text: "cat",
+                        callback_data: 'cat'
+                    }
+                ],
+
+            ]
+        }
+    })
+    });
+
+    bot.action('dog', ctx => {
+        ctx.reply('dog👍')
+    
+    })
+    bot.action('cat', ctx => {
+        ctx.reply('cat👍')
+    })
 
 module.exports = bot
 
-// guarded-anchorage-02020
-// https://guarded-anchorage-02020.herokuapp.com/
